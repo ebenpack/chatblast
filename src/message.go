2f3da5abe3df021c62801f42af94e9f2480fac80
ebenpack <ebenpackwood@gmail.com>
2015-08-23 15:10:44 -0400

    Add Text field

--- src/message.go
+++ src/message.go
@@ -7,7 +7,9 @@ type messageData struct {
 
 type Message struct {
 	Msg    []messageData `json:"data,omitempty"`
+	Text   string        `json:"text,omitempty"`
 	Cmd    string        `json:"cmd,omitempty"`
 	RoomId string        `json:"rid,omitempty"`
 	UserId string        `json:"uid,omitempty"`
+	User   *User         `json:"-"`
 }
