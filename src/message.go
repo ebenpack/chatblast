6756cf9cc2006239697a4f130c744f0fb6cc253b
ebenpack <ebenpackwood@gmail.com>
2015-08-29 18:11:13 -0400

    Update JSON serialization

--- src/message.go
+++ src/message.go
@@ -1,13 +1,13 @@
 package chatblast
 
 type messageData struct {
-	Name string `json:"name,omitempty"`
-	Value  string `json:"value,omitempty"`
+	Name  string `json:"name,omitempty"`
+	Value string `json:"value,omitempty"`
 }
 
 type Message struct {
 	Msg    []messageData `json:"data,omitempty"`
-	Text   string        `json:"text,omitempty"`
+	Text   string        `json:"txt,omitempty"`
 	Cmd    string        `json:"cmd,omitempty"`
 	RoomId string        `json:"rid,omitempty"`
 	UserId string        `json:"uid,omitempty"`
