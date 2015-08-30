8c7571dab191bcdadfaeae01986eca770122ee24
ebenpack <ebenpackwood@gmail.com>
2015-08-29 20:50:20 -0400

    go fmt

--- src/user.go
+++ src/user.go
@@ -7,11 +7,11 @@ import (
 )
 
 type User struct {
-	Name    string          `json:"name,omitempty"`
-	Id      string          `json:"id,omitempty"`
-	Connected int64		`json:"connected,omitempty"`
-	Channel chan *Message   `json:"-"`
-	Conn    *websocket.Conn `json:"-"`
+	Name      string          `json:"name,omitempty"`
+	Id        string          `json:"id,omitempty"`
+	Connected int64           `json:"connected,omitempty"`
+	Channel   chan *Message   `json:"-"`
+	Conn      *websocket.Conn `json:"-"`
 }
 
 func (u *User) Listen() {
@@ -27,11 +27,11 @@ func (u *User) Tell(msg *Message) {
 func NewUser(name string, conn *websocket.Conn) *User {
 	now := time.Now().Unix()
 	newUser := &User{
-		Name:    name,
-		Id:      GUID(),
+		Name:      name,
+		Id:        GUID(),
 		Connected: now,
-		Channel: make(chan *Message),
-		Conn:    conn,
+		Channel:   make(chan *Message),
+		Conn:      conn,
 	}
 	log.Println("New user", name, "created")
 	go newUser.Listen()
