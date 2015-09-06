5ab274835d4943c16bcedb5fe4e4472eeca095b0
ebenpack <ebenpackwood@gmail.com>
2015-09-06 12:44:37 -0400

    Add compress gulp task

--- js/gulpfile.js
+++ js/gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var reactify = require('reactify');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
 
 gulp.task('reactor', function() {
     var b = browserify({standalone: 'chatblast'});
@@ -14,6 +16,13 @@ gulp.task('reactor', function() {
 
 gulp.task('watch', function(){
     gulp.watch('./src/**/*.{js,jsx}', ['reactor']);
+});
+
+gulp.task('compress', function() {
+  return gulp.src('./dist/bundle.js')
+    .pipe(uglify())
+    .pipe(rename('bundle.min.js'))
+    .pipe(gulp.dest('./dist/'));
 });
 
 gulp.task('build', ['reactor']);
