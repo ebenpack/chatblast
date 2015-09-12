322df01795807d4aaaf4515fee999b919a6fd11b
ebenpack <ebenpackwood@gmail.com>
2015-09-12 12:13:18 -0400

    Factor out ChatInput

---
+++ js/src/ChatInput.jsx
@@ -0,0 +1,107 @@
+var React = require('react');
+var Chat = require('./Chat.jsx');
+var Actions = require('./Actions');
+
+var ChatInput = React.createClass({
+    handleDrop: function(e) {
+        var chatBox = React.findDOMNode(this.refs.chatBox);
+        e.stopPropagation();
+        e.preventDefault();
+        var length = e.dataTransfer.files.length;
+        if (length > 0) {
+            var count = 0;
+            Array.prototype.forEach.call(e.dataTransfer.files, function(curr) {
+                var reader = new FileReader();
+                reader.onload = function(e) {
+                    var img = new Image();
+                    img.src = e.target.result;
+                    chatBox.appendChild(img);
+                };
+                reader.readAsDataURL(curr);
+            });
+        } else {
+            var file_uris = e.dataTransfer.getData("text/uri-list");
+            file_uris.split('\n').
+            filter(function(curr) {
+                return curr[0] !== "" && curr[0] !== "#";
+            }).
+            forEach(function(curr) {
+                var img = new Image();
+                img.src = curr;
+                chatBox.appendChild(img);
+            });
+        }
+    },
+    handleKeyDown: function(e) {
+        if (e.keyCode === 13 && !e.shiftKey) {
+            e.preventDefault();
+        }
+    },
+    handleKeyUp: function(e) {
+        var chatBox = React.findDOMNode(this.refs.chatBox);
+        if (this.props.readyState === 1) {
+            if (e.keyCode === 13 && !e.shiftKey) {
+                e.preventDefault();
+                this.submit();
+            }
+        } else {
+            chatBox.textContent = "";
+        }
+    },
+    submit: function() {
+        var chatBox = React.findDOMNode(this.refs.chatBox);
+
+        function serialize(prev, curr) {
+            if (typeof curr === "string") {
+                prev.push({
+                    name: "text",
+                    value: curr
+                });
+                return prev;
+            } else if (curr.nodeName === "#text") {
+                prev.push({
+                    name: "text",
+                    value: curr.textContent
+                });
+                return prev;
+            } else if (curr.nodeName && curr.nodeName.toLowerCase() === "div") {
+                prev.push.apply(prev, Array.prototype.reduce.call(curr.childNodes, serialize, []));
+                return prev;
+            } else if (curr.nodeName && curr.nodeName.toLowerCase() === "img") {
+                prev.push({
+                    name: "img",
+                    value: curr.src
+                });
+                return prev;
+            } else {
+                return prev;
+            }
+        }
+        chatBox.normalize();
+        var msg = Array.prototype.reduce.call(chatBox.childNodes, serialize, []);
+        if (msg.length !== 0) {
+            Actions.chatBlast({
+                "cmd": "msg",
+                "data": msg
+            });
+            chatBox.textContent = '';
+        }
+    },
+    handleDragEnter: function(e) {
+        e.stopPropagation();
+        e.preventDefault();
+    },
+    handleDragOver: function(e) {
+        e.stopPropagation();
+        e.preventDefault();
+
+    },
+    render: function() {
+        var contentEditable = this.props.readyState === 1 ? "true" : "false";
+        return (
+            <div className="textInput" contentEditable={contentEditable} onDragEnter={this.handleDragEnter} onDragOver={this.handleDragOver} onDrop={this.handleDrop} onKeyUp={this.handleKeyUp} onKeyDown={this.handleKeyDown} placeholder="Type a chatblast!" ref="chatBox"></div>
+        );
+    }
+});
+
+module.exports = ChatInput;
