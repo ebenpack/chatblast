8424e934c475f9f9ed5cb555b9c07cdcf151bfa7
ebenpack <ebenpackwood@gmail.com>
2015-09-28 16:40:17 -0400

    Add debounce function

--- static/js/src/util.js
+++ static/js/src/util.js
@@ -5,4 +5,25 @@ function convertFromEpoch(timestamp) {
     return d.toLocaleTimeString();
 }
 
+function debounce(fn, timeout){
+    var lastCalled,
+        timeoutId;
+    return function() {
+        lastCalled = Date.now();
+        var context = this;
+        function debounced(){
+            var now = Date.now();
+            var elapsed = now - lastCalled;
+            if (elapsed > timeout){
+                fn.apply(context, arguments);
+            } else {
+                timeoutId = setTimeout(debounced, elapsed);
+            }
+        }
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(debounced, timeout);
+    };
+}
+
 exports.convertFromEpoch = convertFromEpoch;
+exports.debounce = debounce;
