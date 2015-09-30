6656b36f8073361087051c8b2d747b803189ab04
ebenpack <ebenpackwood@gmail.com>
2015-09-29 20:07:59 -0400

    Remove unused imports

--- static/js/src/Commands.jsx
+++ static/js/src/Commands.jsx
@@ -1,7 +1,4 @@
 var React = require('react');
-var Reflux = require('reflux');
-var Store = require('./Store');
-var Actions = require('./Actions');
 var Connect = require('./Connect.jsx');
 
 var Commands = React.createClass({
